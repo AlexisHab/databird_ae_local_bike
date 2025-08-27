47409bc44290d49beed0360dee0d139e87d64648
Alex Is <thisismygarbagemail@proton.me>
2025-08-27 16:11:45 +0000

    create stg_stores file

---
+++ models/1-stg/stg_stores.sql
@@ -0,0 +1,10 @@
+select
+    cast(store_id as int) as store_id, 
+    cast(store_name as string) as store_name, 
+    cast(phone as string) as phone,
+    cast(email as string) as email,
+    cast(street as string) as street,
+    cast(city as string) as city,
+    cast(state as string) as state,
+    cast(zip_code as int) as zip_code,
+from {{ source('local_bike','stores')}}
