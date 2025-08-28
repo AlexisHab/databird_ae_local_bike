82c52a127d43e8444db0e61c819ab5b37a27692e
Alex Is <thisismygarbagemail@proton.me>
2025-08-28 17:30:55 +0000

    fix:data correction on manager id to correctly interpret NULL

--- models/1-stg/stg_staffs.sql
+++ models/1-stg/stg_staffs.sql
@@ -1,5 +1,5 @@
 select
-    cast(staff_id as string) as staff_id, 
+    cast(staff_id as int) as staff_id, 
     cast(first_name as string) as first_name, 
     cast(last_name as string) as last_name,
     cast(phone as string) as phone,
@@ -7,5 +7,5 @@ select
     cast(active as int) as active,
 
     cast(store_id as int) as store_id,
-    cast(manager_id as string) as manager_id,
+    cast(NULLIF(manager_id,'NULL') as int) as manager_id,
 from {{ source('local_bike','staffs')}}
