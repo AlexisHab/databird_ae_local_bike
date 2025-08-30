a67ef261a20ca68c26c162f9ad8096d7eaea61c6
Alex Is <thisismygarbagemail@proton.me>
2025-08-30 21:35:23 +0000

    fix:wrong join condition field

--- models/2-int/_dimensions/int_dim__product_brand_category.sql
+++ models/2-int/_dimensions/int_dim__product_brand_category.sql
@@ -8,5 +8,5 @@ select
     b.brand_name,
     c.category_name
 from {{ ref('stg_production__products') }} p
-left join {{ ref('stg_production__brands') }} b using (product_id)
-left join {{ ref('stg_production__categories') }} c using (product_id)
+left join {{ ref('stg_production__brands') }} b using (brand_id)
+left join {{ ref('stg_production__categories') }} c using (category_id)
