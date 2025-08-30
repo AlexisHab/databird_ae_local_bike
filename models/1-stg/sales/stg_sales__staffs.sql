7a96ea815037d52e165e77d86d7d7432e2a0e8a5
Alex Is <thisismygarbagemail@proton.me>
2025-08-30 20:26:01 +0000

    add full names to staff - concat first & last name

--- models/1-stg/sales/stg_sales__staffs.sql
+++ models/1-stg/sales/stg_sales__staffs.sql
@@ -2,6 +2,7 @@ select
     cast(staff_id as int) as staff_id, 
     cast(first_name as string) as first_name, 
     cast(last_name as string) as last_name,
+    concat(cast(first_name as string)," ",cast(last_name as string)) as full_name,
     cast(phone as string) as phone,
     cast(email as string) as email,
     cast(active as int) as active,
