71b6b77db1106f033e8bace6dc3acd2bc18390d3
Alex Is <thisismygarbagemail@proton.me>
2025-08-30 21:25:18 +0000

    create intermediate dim product to bring brand & category to simplify modeling as they are not used elsewhere

---
+++ models/2-int/_dimensions/int_dim__product_brand_category.sql
@@ -0,0 +1,12 @@
+select 
+    product_id,
+    
+    -- Attributes
+    p.product_name,
+    p.model_year,
+    p.list_price,
+    b.brand_name,
+    c.category_name
+from {{ ref('stg_production__products') }} p
+left join {{ ref('stg_production__brands') }} b using (product_id)
+left join {{ ref('stg_production__categories') }} c using (product_id)
